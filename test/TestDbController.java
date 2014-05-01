f5ff469fe68053c691b92becd777b1e1c18efa6f
sashapodgoreanu <p.alxzeta@gmail.com>
2014-05-02 01:23:43 +0200

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	test/CryptoHelper.java

--- test/TestDbController.java
+++ test/TestDbController.java
@@ -1,5 +1,6 @@
 
 import cryptohelper.bean.Messaggio;
+import cryptohelper.bean.Studente;
 import cryptohelper.service.DBController;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -19,11 +20,13 @@ public class TestDbController {
     public static void main(String args[]) {
 
         Messaggio m = new Messaggio(21, "testo", "testoCifrato", "lingua", "titolo", true, true);
+        Studente st = new Studente("Alexandru","Podgoreanu","sasha","1234");
         try {
             DBController.registerDB();
             DBController.createTables();
             m.salva();
             m.elimina();
+            st.salva();
 
         } catch (SQLException ex) {
             Logger.getLogger(TestDbController.class.getName()).log(Level.SEVERE, null, ex);
