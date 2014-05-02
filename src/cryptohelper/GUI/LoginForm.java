755898b82e0605006233efa3159a972260b2d571
Giulio <Giulio@192.168.0.128>
2014-05-02 13:49:20 +0200

    Merge origin/master
    Conflicts:
    	test/TestDbController.java

--- src/cryptohelper/GUI/LoginForm.java
+++ src/cryptohelper/GUI/LoginForm.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Classe Login Form - Gestisce il form per l'accesso al servizio da parte degli utenti
+
 package cryptohelper.GUI;
 
 import cryptohelper.data.Studente;
@@ -47,7 +44,7 @@ public class LoginForm extends JFrame {
         this.pack();
         this.setVisible(true);
         this.setResizable(false);
-        this.setLocationRelativeTo(null);
+        this.setLocationRelativeTo(null);   //centra la finestra sullo schermo
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
