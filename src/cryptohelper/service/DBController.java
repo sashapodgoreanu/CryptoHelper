61a6bc52e7790c37a370ba65040eb071e831d404
sashapodgoreanu <p.alxzeta@gmail.com>
2014-05-06 19:58:12 +0200

    Merge origin/master
    Conflicts:
    	src/cryptohelper/com/GUIController.java

--- src/cryptohelper/service/DBController.java
+++ src/cryptohelper/service/DBController.java
@@ -4,9 +4,9 @@ package cryptohelper.service;
 
 import cryptohelper.data.Messaggio;
 import cryptohelper.data.QueryResult;
-import cryptohelper.data.Studente;
 import cryptohelper.data.UserInfo;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -131,7 +131,7 @@ public class DBController {
 
             log.info("DA TOGLIERE I COMENTI DI ST.EXECUTE(QUERRY) QUANDO SI ESEGUE TESTDBCONTROLLER PER LA SECONDA VOLTA!!!!!!!");
             //drop di tutte le tabelle esistenti
-            st.execute("DROP TABLE MessaggiInviati");
+            st.execute("DROP TABLE MessaggiInviati if exists");
             log.info("Tabella MessaggiInviati eliminata!");
             st.execute("DROP TABLE SDCPartners");
             log.info("Tabella SDCPartners eliminata!");
@@ -141,20 +141,20 @@ public class DBController {
             log.info("Tabella Messaggi eliminata!");
             st.execute("DROP TABLE Studenti");
             log.info("Tabella Studenti eliminata!");
-
-            //creazione tabelle
-            st.executeUpdate(queryStudenti);
-            log.info("Tabella Studenti creata!");
-            st.executeUpdate(queryMessaggi);
-            log.info("Tabella Messaggi creata!");
-            st.executeUpdate(querySistemiCifratura);
-            log.info("Tabella SistemiCifratura creata!");
-            st.executeUpdate(querySDCPartners);
-            log.info("Tabella SDCPartners creata!");
-            st.executeUpdate(queryMessaggiInviati);
-            log.info("Tabella MessaggiInviati creata!");
-
-        } catch (Exception e) {
+            /*
+             //creazione tabelle
+             st.executeUpdate(queryStudenti);
+             log.info("Tabella Studenti creata!");
+             st.executeUpdate(queryMessaggi);
+             log.info("Tabella Messaggi creata!");
+             st.executeUpdate(querySistemiCifratura);
+             log.info("Tabella SistemiCifratura creata!");
+             st.executeUpdate(querySDCPartners);
+             log.info("Tabella SDCPartners creata!");
+             st.executeUpdate(queryMessaggiInviati);
+             log.info("Tabella MessaggiInviati creata!");
+             */
+        } catch (SQLException e) {
             log.fatal(e.getMessage());
         } finally {
             disconnect();
@@ -251,7 +251,6 @@ public class DBController {
         return result;
     }
 
-    
     public ArrayList<UserInfo> getDestinatari() throws SQLException {
         connect();
 
@@ -329,5 +328,23 @@ public class DBController {
             System.out.println("Record NOT EDITED with success");
         }
         return (edit == 1 ? true : false);
+    }
+
+    public boolean isTableExist(String sTablename) throws SQLException {
+        connect();
+        if (conn != null) {
+            DatabaseMetaData dbmd = conn.getMetaData();
+            ResultSet rs = dbmd.getTables(null, null, sTablename.toUpperCase(), null);
+            if (rs.next()) {
+                System.out.println("Table " + rs.getString("TABLE_NAME") + "already exists !!");
+                disconnect();
+                return true;
+            } else {
+                disconnect();
+                return false;
+            }
+        }
+        disconnect();
+        return false;
     }
 }
