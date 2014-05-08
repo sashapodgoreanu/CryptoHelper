eece4d1b5216410f4412e0da830097725af8778d
sashapodgoreanu <p.alxzeta@gmail.com>
2014-05-08 19:32:10 +0200

    Merge origin/master
    Conflicts:
    	src/cryptohelper/com/GUIController.java

--- src/cryptohelper/data/MessaggioAstratto.java
+++ src/cryptohelper/data/MessaggioAstratto.java
@@ -1,11 +1,15 @@
 //interfaccia MessaggioAstratto
 package cryptohelper.data;
 
-public interface MessaggioAstratto
-{
+public interface MessaggioAstratto {
+
     public String getTesto();
+
     public String getTestoCifrato();
+
     public String getLingua();
+
     public String getTitolo();
+
     public boolean elimina();
 }
