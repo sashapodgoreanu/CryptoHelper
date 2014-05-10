e29d4f52ad503a95465c07e01e06d9c5200d4d44
Giulio <Giulio@192.168.0.128>
2014-05-10 10:47:45 +0200

    Merge origin/master
    Conflicts:
    	test/TestDbController.java

--- src/cryptohelper/GUI/LoginForm.java
+++ src/cryptohelper/GUI/LoginForm.java
@@ -1,5 +1,4 @@
 //Classe Login Form - Gestisce il form per l'accesso al servizio da parte degli utenti
-
 package cryptohelper.GUI;
 
 import cryptohelper.com.GUIController;
@@ -7,82 +6,87 @@ import java.awt.*;
 import javax.swing.*;
 
 public class LoginForm extends JFrame implements View {
-    
-    JPanel panel;
-    JButton submit;
+
     GUIController gc;
-    JLabel username;
-    JLabel password;
+    JButton submitBtn;
+    JButton registrationBtn;
+    JLabel usernameLabel;
+    JLabel passwordLabel;
     JLabel errorLoginLabel;
     JTextField passwordField;
     JTextField usernameField;
-    
+
     public LoginForm() {
-        this.setPreferredSize(new Dimension(320, 200));
         this.setTitle("CryptoHelper - Login");
-        panel = new JPanel();
-        submit = new JButton("Accedi");
+        this.setSize(new Dimension(320, 200));
+        submitBtn = new JButton("Accedi");
+        registrationBtn = new JButton("Nuovo utente");
         passwordField = new JTextField();
         passwordField.setPreferredSize(new Dimension(200, 24));
         usernameField = new JTextField();
         usernameField.setPreferredSize(new Dimension(200, 24));
-        username = new JLabel("Username");
-        password = new JLabel("Password");
+        usernameLabel = new JLabel("Username");
+        passwordLabel = new JLabel("Password");
         errorLoginLabel = new JLabel("");
         errorLoginLabel.setForeground(Color.RED);
-        panel.setLayout(new FlowLayout());
-        panel.add(username);
-        panel.add(usernameField);
-        panel.add(password);
-        panel.add(passwordField);
-        panel.add(submit);
-        panel.add(errorLoginLabel);
-        this.add(panel);
-        this.pack();
+        this.setLayout(new FlowLayout());
+        this.add(usernameLabel);
+        this.add(usernameField);
+        this.add(passwordLabel);
+        this.add(passwordField);
+        this.add(registrationBtn);
+        this.add(submitBtn);
+        this.add(errorLoginLabel);
         this.setVisible(true);
         this.setResizable(false);
         this.setLocationRelativeTo(null);   //centra la finestra sullo schermo
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.registerController();
     }
-    
+
     @Override
-    public void registerController(){
+    public void registerController() {
         gc = GUIController.getInstance();
         gc.addView(this);
     }
 
+    //METODI GETTER
     public JLabel getErrorLoginLabel() {
         return errorLoginLabel;
     }
 
-    public void setErorLogin(JLabel erorLogin) {
-        this.errorLoginLabel = erorLogin;
-    }
-    
     public JButton getSubmit() {
-        return submit;
+        return submitBtn;
     }
 
-    public void setSubmit(JButton submit) {
-        this.submit = submit;
+    public JButton getRegistration() {
+        return registrationBtn;
     }
-    
-     public String getPassword() {
+
+    public String getUsername() {
+        System.out.println(usernameField.getText());
+        return usernameField.getText();
+    }
+
+    public String getPassword() {
         return passwordField.getText();
+    }
+
+    //METODI SETTER
+    public void setSubmit(JButton submit) {
+        this.submitBtn = submit;
     }
 
     public void setPasswordField(String pwd) {
         this.passwordField.setText(pwd);
     }
 
-    public String getUsername() {
-        System.out.println(usernameField.getText());
-        return usernameField.getText();
-        
-    }
-
     public void setUsernameField(String usr) {
         this.usernameField.setText(usr);
     }
+
+    public void setErorLogin(JLabel erorLogin) {
+        this.errorLoginLabel = erorLogin;
+    }
+
 }
