c15073be6a70fb10c9048820d5d7f939a051adbd
Giulio <Giulio@192.168.0.128>
2014-05-10 10:24:22 +0200

    Merge origin/master
    Conflicts:
    	test/TestDbController.java

--- src/cryptohelper/data/MessaggioAstratto.java
+++ src/cryptohelper/data/MessaggioAstratto.java
@@ -4,12 +4,8 @@ package cryptohelper.data;
 public interface MessaggioAstratto {
 
     public String getTesto();
-
     public String getTestoCifrato();
-
     public String getLingua();
-
     public String getTitolo();
-
     public boolean elimina();
 }
