34ba1df7334ca572b41ba1f5a56a93281ed2b946
Giulio <Giulio@192.168.1.128>
2014-05-12 18:45:11 +0200

    Merge origin/master
    Conflicts:
    	test/TestDbController.java

---
+++ src/cryptohelper/GUI/InboxPanel.java
@@ -0,0 +1,161 @@
+//Finestra principale della GUI
+package cryptohelper.GUI;
+
+import cryptohelper.com.GUIController;
+import cryptohelper.data.MessaggioDestinatario;
+import cryptohelper.data.MessaggioMittente;
+import cryptohelper.data.Studente;
+import cryptohelper.data.UserInfo;
+import java.util.ArrayList;
+import java.util.Vector;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+
+public class InboxPanel extends JPanel implements View {
+
+    JPanel topPanel;         //pannello in alto
+    JPanel leftPanel;        //pannello a sinistra
+    JPanel rightPanel;       //pannello a destra
+    JPanel bottomPanel;      //pannello in basso
+    JButton viewMessageBtn;
+    JLabel targetListLabel;
+    JLabel msgTitlelLabel;
+    JLabel messageTextLabel;
+    JTextField titoloMessaggioField;
+    JList elencoDestinatari;            //visualizza la lista dei destinatariArrLst
+    JList elencoMessaggiRicevuti;
+    JTextArea corpoMessaggio;
+    JScrollPane scrollPane;
+    ArrayList<MessaggioDestinatario> mittentiMessaggiArrLst; //elenco mittenti
+
+    public InboxPanel(ArrayList<MessaggioDestinatario> mittentiMessaggiArrLst) {
+        this.mittentiMessaggiArrLst = mittentiMessaggiArrLst;
+        this.init();
+    }
+
+    //inizializza il pannello
+    private void init() {
+        System.out.println("Inizzializzazione Pannello Inbox...");   //comunicazione di controllo per i log
+
+        //INIT DEI PANNELLI E DEI LAYOUT
+        this.setLayout(new BorderLayout());
+        topPanel = new JPanel();
+        leftPanel = new JPanel();
+        rightPanel = new JPanel();
+        bottomPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout());
+        leftPanel.setLayout(new BorderLayout());
+        rightPanel.setLayout(new BorderLayout());
+        bottomPanel.setLayout(new FlowLayout());
+
+        //INIT DEI CONTROLLI
+        viewMessageBtn = new JButton("Visualizza Messaggio");
+        targetListLabel = new JLabel("Messaggi ricevuti:");
+        messageTextLabel = new JLabel("Testo del messaggio:");
+        msgTitlelLabel = new JLabel("Tiolo messaggio: ");
+        titoloMessaggioField = new JTextField(21);
+        elencoMessaggiRicevuti = new JList(new Vector<MessaggioDestinatario>(mittentiMessaggiArrLst));
+        elencoMessaggiRicevuti.setCellRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                Component renderer = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                if (renderer instanceof JLabel && value instanceof MessaggioDestinatario) {
+                    MessaggioDestinatario temp = (MessaggioDestinatario) value;
+                    ((JLabel) renderer).setText(temp.getTitolo() + " " + temp.getId());
+                }
+                return renderer;
+            }
+        });
+        elencoMessaggiRicevuti.setSelectedIndex(0);
+        scrollPane = new JScrollPane();
+        scrollPane.setViewportView(elencoMessaggiRicevuti);
+        corpoMessaggio = new JTextArea();
+        corpoMessaggio.setSize(new Dimension(540, 250));
+        corpoMessaggio.setLineWrap(true);
+        corpoMessaggio.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY)); // aggiunge un bordo alla textArea
+
+        //AGGIUNTA DEI CONTROLLI AI PANNELLI        
+        topPanel.add(msgTitlelLabel);
+        topPanel.add(titoloMessaggioField);
+        leftPanel.add(messageTextLabel, BorderLayout.NORTH);
+        leftPanel.add(corpoMessaggio, BorderLayout.CENTER);
+        rightPanel.add(targetListLabel, BorderLayout.NORTH);
+        rightPanel.add(scrollPane, BorderLayout.CENTER);
+        bottomPanel.add(viewMessageBtn);
+
+        //AGGIUNTA DEI PANNELLI
+        this.add(topPanel, BorderLayout.NORTH);
+        this.add(leftPanel, BorderLayout.WEST);
+        this.add(rightPanel, BorderLayout.EAST);
+        this.add(bottomPanel, BorderLayout.SOUTH);
+
+        //REGISTRAZIONE VISTA NEL COTROLLER
+        registerController();
+    }
+
+    public void modificaCorpoMessaggio(String testo) {
+        corpoMessaggio.setText(testo);
+    }
+
+    @Override
+    public void registerController() {
+        GUIController gc = GUIController.getInstance();
+        gc.addView(this);
+    }
+
+    //METODI GETTER
+    public JList getDestinatariCC() {
+        return elencoDestinatari;
+    }
+
+    public Object getSelectedDestinatario() {
+        return elencoDestinatari.getSelectedValue();
+    }
+
+    public JList getElencoDestinatari() {
+        return elencoDestinatari;
+    }
+
+    public void setElencoDestinatari(JList elencoDestinatari) {
+        this.elencoDestinatari = elencoDestinatari;
+    }
+
+    public JList getElencoMessaggiRicevuti() {
+        return elencoMessaggiRicevuti;
+    }
+
+    public String getTittoloMessaggioField() {
+        return titoloMessaggioField.getText();
+    }
+
+    public String getCorpoMessaggio() {
+        return corpoMessaggio.getText();
+    }
+
+    public JButton getVisualizzaMessaggioBtn() {
+        return viewMessageBtn;
+    }
+
+    //METODI SETTER
+    public void setCorpoMessaggio(String msg) {
+        this.corpoMessaggio.setText(msg);
+    }
+
+    public void setTittoloMessaggioField(String titolo) {
+        this.titoloMessaggioField.setText(titolo);
+    }
+
+    public void setDestinatariCC(JList destinatariCC) {
+        this.elencoDestinatari = destinatariCC;
+    }
+
+    public ArrayList<MessaggioDestinatario> getMittentiMessaggiArrLst() {
+        return mittentiMessaggiArrLst;
+    }
+
+    public void setMittentiMessaggiArrLst(ArrayList<MessaggioDestinatario> bozzeArayLst) {
+        this.mittentiMessaggiArrLst = bozzeArayLst;
+    }
+
+}
