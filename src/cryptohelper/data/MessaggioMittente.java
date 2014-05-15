2ab0966abd1830018bf70350d9e2dc76782ed956
Giulio <Giulio@192.168.1.128>
2014-05-15 09:47:16 +0200

    Merge origin/master
    Conflicts:
    	test/TestDbController.java

--- src/cryptohelper/data/MessaggioMittente.java
+++ src/cryptohelper/data/MessaggioMittente.java
@@ -1,13 +1,16 @@
 //interfaccia MessaggioMittente
-
 package cryptohelper.data;
 
-public interface MessaggioMittente extends MessaggioAstratto
-{
+public interface MessaggioMittente extends MessaggioAstratto {
+
     public boolean isBozza();
+
     public boolean salva();
+
     public void cifra();
+
     public void setMittente(UserInfo user);
+
     public UserInfo getMittente();
-    
+
 }
