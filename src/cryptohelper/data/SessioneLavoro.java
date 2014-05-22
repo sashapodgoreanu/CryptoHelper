7d80688be15f72ef77837df17e72c93959ea6c24
st106342 <st106342@VONNEUMANN23.labinfo.educ.di.unito.it>
2014-05-22 17:06:20 +0200

    Merge origin/master
    Conflicts:
    	src/cryptohelper/GUI/PanelloPrincipale.java

--- src/cryptohelper/data/SessioneLavoro.java
+++ src/cryptohelper/data/SessioneLavoro.java
@@ -16,7 +16,15 @@
 
 package cryptohelper.data;
 
+import cryptohelper.com.COMController;
+import cryptohelper.interfaces.MessaggioMittente;
+import cryptohelper.service.DBController;
+import static java.lang.StrictMath.log;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,26 +33,130 @@ import java.util.Date;
 public class SessioneLavoro {
     
     int idSessione;
-    int idUtente;
+    UserInfo utente;
     Date ultimaModifica;
-    int idAlbero;
-    int idMessaggioIntercettato;
-    int idSoluzione;
+     // alberoipotesi
+    Messaggio messaggioIntercettato;
+    // soluzione
         
      //COSTRUTTORE I
-    public SessioneLavoro (int id, int studente, int albero, int messaggio, int soluzione) {
+    public SessioneLavoro (int id, UserInfo studente, AlberoIpotesi albero, Messaggio messaggio /* soluzione*/) {
     
         idSessione = id;
-        idUtente = studente;
-        ultimaModifica = new Date(); //inizializza ultimaModifica con la data attuale.
-        idAlbero = albero;
-        idMessaggioIntercettato = messaggio;
-        idSoluzione = soluzione;
+        utente=studente;
+        messaggioIntercettato = messaggio;
+        
+        ultimaModifica = new Date(); //inizializza ultimaModifica con la data attuale.       
+    }    
+    
+    public int getIdSessione() {
+        return idSessione;
+    }
+
+    public void setIdSessione(int idSessione) {
+        this.idSessione = idSessione;
+    }
+
+    public UserInfo getUtente() {
+        return utente;
+    }
+
+    public void setUtente(UserInfo utente) {
+        this.utente = utente;
+    }
+
+    public Date getUltimaModifica() {
+        return ultimaModifica;
+    }
+
+    public void setUltimaModifica(Date ultimaModifica) {
+        this.ultimaModifica = ultimaModifica;
+    }
+
+    public Messaggio getMessaggioIntercettato() {
+        return messaggioIntercettato;
+    }
+
+    public void setMessaggioIntercettato(Messaggio messaggioIntercettato) {
+        this.messaggioIntercettato = messaggioIntercettato;
+    }  
+
+    //Salva una sessione nella tabella SESSIONELAVORO del db. Restituisce TRUE se l'oparazione va a buon fine
+    public boolean salva() {
+        boolean result = false;
+        DBController dbc = DBController.getInstance();
+        String queryInsert = "INSERT INTO Sessionelavoro(Id_Utente, Id_Messaggio_Intercettato, Ultima_Modifica)"
+                + "VALUES("
+                + this.getUtente().getId()
+                + ","
+                + this.getMessaggioIntercettato().getId()
+                + ",'"
+                + this.getUltimaModifica()
+                + "')";
+        String querryUpdate = "UPDATE MESSAGGI"
+                + " Id_Utente = '" + this.getUtente().getId()
+                + "',"
+                + " Id_Messaggio_Intercettato = '"+ this.getMessaggioIntercettato().getId()
+                + "',"
+                + " Ultima_Modifica = "+ this.getUltimaModifica()
+                + "',"
+                + " WHERE ID = " + this.getIdSessione();
+        try {
+            //un nuovo messaggo
+            if (this.getIdSessione() == 0) {
+                int newID = dbc.executeUpdateAndReturnKey(queryInsert);
+                //se newID = -1 allora è stato un errore nel inserimento nel db;
+                if (newID != -1) {
+                    this.idSessione = newID;
+                    System.out.println("INFO DATA:" + this.getClass() + "." + Thread.currentThread().getStackTrace()[1].getMethodName() + ": Aggiunto con successo " + this.toString());
+                    System.out.println(true);
+                    return true;
+
+                }
+                if (newID == -1 && this.idSessione != 0) {
+                    System.out.println(false);
+                    //errore nel inserimento
+                    return false;
+                }
+                //aggiornamento di un messaggio
+            } else {
+                result = dbc.executeUpdate(querryUpdate);
+                System.out.println("INFO DATA:" + this.getClass() + "." + Thread.currentThread().getStackTrace()[1].getMethodName() + "Aggiornato: " + this.toString());
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return result;
+    }
+public SessioneLavoro (int id, int studente, int albero, int messaggio, int soluzione) {
+    
+        idSessione = id;
+        utente.setId(studente);
+        ultimaModifica = new Date(); //inizializza ultimaModifica con la data attuale.       
+    }    
+
+
+    //Preleva l'elenco dei messaggi inviati dallo studente indicato
+    public static ArrayList<SessioneLavoro> caricaSessioni(int idStudente) {
+        String query = "SELECT * FROM SessioneLavoro WHERE ID_Utente = " + idStudente;
+        QueryResult qr = null;
+        ArrayList<SessioneLavoro> sessioni = new ArrayList<>();
+        try {
+            qr = DBController.getInstance().executeQuery(query);
+            while (qr.next()) {
+                UserInfo user = UserInfo.getUserInfo(idStudente);
+                SessioneLavoro temp = new SessioneLavoro(qr.getInt("ID"), qr.getString("Testo"), qr.getString("TestoCifrato"),
+                        qr.getString("Lingua"), qr.getString("Titolo"), Boolean.parseBoolean(qr.getString("Bozza")), Boolean.parseBoolean(qr.getString("Letto")),
+                        user);
+                sessioni.add(temp);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(COMController.class.getName()).log(Level.SEVERE, null, ex.getMessage());
+        }
+        return sessioni;
     }
     
-    // metodo che salva una sessione di lavoro.
-    public boolean salva () {
-           
-        return true;
-    }
+    
+    
 }
