dd8f81398894f27541ec34a90ef88dcc30db91b6
st116629 <st116629@VONNEUMANN24.labinfo.educ.di.unito.it>
2014-05-22 17:13:46 +0200

    Merge origin/master

--- src/cryptohelper/GUI/UC2/CaricaSessionePanel.java
+++ src/cryptohelper/GUI/UC2/CaricaSessionePanel.java
@@ -27,14 +27,13 @@ public class CaricaSessionePanel extends JPanel implements View {
     JButton okBtn;
     ArrayList<Messaggio> elencoSessioniArrLst; //elenco destintari dei messaggi
 
-    
     //TO DO creare classe sessione e passare array come parametro qui
     public CaricaSessionePanel(/*ArrayList<Sessione> elencoSessioni*/) {
         topPanel = new JPanel();
         leftPanel = new JPanel();
         rightPanel = new JPanel();
         bottomPanel = new JPanel();
-       // this.elencoSessioniArrLst = elencoSessioni;
+        // this.elencoSessioniArrLst = elencoSessioni;
         this.init();
     }
 
@@ -55,34 +54,33 @@ public class CaricaSessionePanel extends JPanel implements View {
         messageTextLabel = new JLabel("Informazioni sessione:");
         eliminaSessioneBtn = new JButton("Elimina sessione");
         okBtn = new JButton("Avanti");
-        /*
-        elencoSessioni = new JList(new Vector<Sessione>(elencoSessioniArrLst));
+        elencoSessioni = new JList(new Vector<SessioneLavoro>(elencoSessioniArrLst));
         elencoSessioni.setCellRenderer(new DefaultListCellRenderer() {
             @Override
             public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
                 Component renderer = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-                if (renderer instanceof JLabel && value instanceof Sessione) {
-                    Sessione temp = (Sessione) value;
+                if (renderer instanceof JLabel && value instanceof SessioneLavoro) {
+                    SessioneLavoro temp = (SessioneLavoro) value;
                     ((JLabel) renderer).setText(temp.getTitolo());
                 }
                 return renderer;
             }
         });
- */
-//        elencoSessioni.setSelectedIndex(0);
-//        elencoSessioni.setPreferredSize(new Dimension(165, 250));
+        elencoSessioni.setSelectedIndex(0);
+
         scrollPane = new JScrollPane();
         scrollPane.setViewportView(elencoSessioni);
+        scrollPane.setPreferredSize(new Dimension(165, 250));
         infoSessione = new JTextPane();
         infoSessione.setPreferredSize(new Dimension(600, 250));
         infoSessione.setContentType("text/html"); //consente formattazione html
         infoSessione.setEditable(false); //rende in sola lettura il campo con il testo del messaggio
         Border b = BorderFactory.createLineBorder(Color.GRAY);  //crea un bordo al controllo
-        infoSessione.setBorder(BorderFactory.createCompoundBorder(b,BorderFactory.createEmptyBorder(0, 10, 0, 10))); //assegna un margine al controllo
-//        Messaggio index0 = (Messaggio) elencoSessioni.getSelectedValue();
-    //    if (index0 != null) {
-    //        infoSessione.setText(new HtmlVisitor().visit(index0));
-    //    }
+        infoSessione.setBorder(BorderFactory.createCompoundBorder(b, BorderFactory.createEmptyBorder(0, 10, 0, 10))); //assegna un margine al controllo
+        SessioneLavoro index0 = (SessioneLavoro) elencoSessioni.getSelectedValue();
+        if (index0 != null) {
+            infoSessione.setText(new HtmlVisitor().visit(index0));
+        }
 
         //AGGIUNTA DEI CONTROLLI AI PANNELLI        
         leftPanel.add(messageTextLabel, BorderLayout.NORTH);
@@ -113,12 +111,12 @@ public class CaricaSessionePanel extends JPanel implements View {
         return elencoSessioni;
     }
 
-   public JTextPane getInfoSessione() {
+    public JTextPane getInfoSessione() {
         return infoSessione;
     }
-        
+
     public JButton getokBtn() {
         return okBtn;
     }
-    
+
 }
