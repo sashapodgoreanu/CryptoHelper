66a79d88caf46d0a0d548df7fb5a5d9c7c4fc426
Luigi <Luigi@Luigi-PC>
2014-05-25 11:13:58 +0200

    Revert "Merge origin/master"
    This reverts commit 46f971490c379b8e5ebd8e4643fd50dd7a9901f0.

---
+++ src/cryptohelper/proxy/testProxy1.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2014 Luigi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cryptohelper.proxy;
+
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Luigi
+ */
+public class testProxy1 {
+
+    public static void main(String[] args) {
+
+        
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        File file = new File("bgItaliano.txt");
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String text = null;
+
+            char i;
+            while ((text = reader.readLine()) != null) {       
+                i = ((char)Integer.parseInt(text));      
+                System.out.println(i);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+
+//print out the list
+        System.out.println(list);
+
+    }
+
+}
