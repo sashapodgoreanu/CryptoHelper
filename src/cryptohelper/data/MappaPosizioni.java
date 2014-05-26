b4868af9a8526d61598b121d8cbf32b196afb77e
sashapodgoreanu <p.alxzeta@gmail.com>
2014-05-26 22:23:06 +0200

    Revert "Merge origin/master"
    This reverts commit 46f971490c379b8e5ebd8e4643fd50dd7a9901f0.

--- src/cryptohelper/data/MappaPosizioni.java
+++ src/cryptohelper/data/MappaPosizioni.java
@@ -4,33 +4,37 @@ public class MappaPosizioni {
 
     private MappaPosCarattere[] mappa;
 
-    public MappaPosizioni() {
-        mappa = new MappaPosCarattere[512];
+    public MappaPosizioni(String testoCifrato) {
+        inizializza(testoCifrato);
     }
 
     public String executeMossa(Mossa mossa, String testoLavoro) {
-        throw new UnsupportedOperationException();
+
+        return null;
+    }
+
+    private void updateMappa(Mossa mossa) {
+
     }
 
     public void inizializza(String testoCifrato) {
+        mappa = new MappaPosCarattere[256];
         testoCifrato = testoCifrato.toUpperCase(); //trasforma tutti i carratteri in caratteri minuscoli
         for (int i = 0; i < testoCifrato.length(); i++) {
-            if (testoCifrato.charAt(i) >= 'A' || testoCifrato.charAt(i) <= 'Z')
+            if (testoCifrato.charAt(i) >= 'A' || testoCifrato.charAt(i) <= 'Z') {
                 addPos(testoCifrato.charAt(i), i);
+            }
         }
     }
-    
-    public Mossa createMossaUndo(char ch2){
-        char ch1 = mappa[(int)ch2].getCarattere();
-        mappa[(int)ch2].setIsUpercase(true);
+
+    public Mossa createMossaUndo(char ch2) {
+        //System.out.println("createMossaUndoUppercase " + ch2);
+        char ch1 = mappa[(int) ch2].getCarattere();
+        if ((ch1 - 32) == ch2) {
+            mappa[(int) ch2].setIsUpercase(true);//
+        }
         return new Mossa(ch1, ch2);
     }
-
-   // public char get
-
-    //map(char c) {
-        //to-do
- //   }
 
     public MappaPosCarattere[] getMappa() {
         return mappa;
@@ -59,8 +63,9 @@ public class MappaPosizioni {
     public String toString() {
         StringBuilder a = new StringBuilder();
         for (int i = 'A'; i <= 'Z'; i++) {
-            if (this.mappa[i] != null)
-                a.append(mappa[i].toString()).append("\n");
+            if (this.mappa[i] != null) {
+                a.append("Carattere immutabile = ").append((char) i).append(", ").append(mappa[i].toString()).append("\n");
+            }
         }
         return a.toString();
     }
