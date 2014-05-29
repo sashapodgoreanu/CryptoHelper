4ee9cb4fd2ff271c80abd03820fdfb5cdbb4bd40
Giulio <Giulio@192.168.1.128>
2014-05-29 10:03:51 +0200

    Merge origin/master
    Conflicts:
    	test/TestDbController.java

--- test/cryptohelper/data/CifratoreTest.java
+++ test/cryptohelper/data/CifratoreTest.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 Sasha Alexandru Podgoreanu.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package cryptohelper.data;
 
 import org.junit.After;
@@ -23,32 +7,29 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Sasha Alexandru Podgoreanu
- */
 public class CifratoreTest {
+
     Mappatura mappa;
-    
+
     public CifratoreTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-       mappa = new Mappatura();
-       char[] inversemap = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-       mappa.setMappaInversa(inversemap);
-       
+        mappa = new Mappatura();
+        char[] inversemap = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        mappa.setMappaInversa(inversemap);
+
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -64,12 +45,10 @@ public class CifratoreTest {
         assertFalse("zzz".equals(Cifratore.cifraMonoalfabetica(mappa, "aaa")));
         assertFalse("false".equals(Cifratore.cifraMonoalfabetica(mappa, "true")));
     }
-    
 
     /**
      * Test of decifraMonoalfabetica method, of class Cifratore.
      */
-    
     @Test
     public void testDecifraMonoalfabetica() {
         System.out.println("Test of decifraMonoalfabetica method, of class Cifratore.");
@@ -77,5 +56,5 @@ public class CifratoreTest {
         assertFalse("aa".equals(Cifratore.decifraMonoalfabetica(mappa, "aaa")));
         assertFalse("zzz".equals(Cifratore.decifraMonoalfabetica(mappa, "aaa")));
     }
-    
+
 }
